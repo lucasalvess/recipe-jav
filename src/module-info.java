9fe3915e65f180403608e0f31b146664e207ee36
felipetiburcioDASA <felipe.tiburcio@dasa.com.br>
2019-11-21 15:10:06 -0300

    Transforming in java web project

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module TESTE {
+	exports br.com.dasa.recipe.config;
+	exports br.com.dasa.recipe.dao;
+	exports br.com.dasa.recipe.model;
+	exports br.com.dasa.recipe.service;
+	exports br.com.dasa.recipe.servlet;
+
+	requires java.sql;
+}
