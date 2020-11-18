1563995bcff1d8f1c0273db2ad21f4d15fa32953
lucasalvess <lcluphus@gmail.com>
2020-11-17 21:56:05 -0300

    Fixing controller + creating docker structure

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module TESTE {
-	exports br.com.dasa.recipe.config;
-	exports br.com.dasa.recipe.dao;
-	exports br.com.dasa.recipe.model;
-	exports br.com.dasa.recipe.service;
-	exports br.com.dasa.recipe.servlet;
-
-	requires java.sql;
-}
