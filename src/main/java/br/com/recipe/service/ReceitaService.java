342c9b5dbc814dc12dc338711f90f0c08ab4d905
lucasalvess <lcluphus@gmail.com>
2021-01-19 22:29:44 -0300

    Using constructor to receive instances

--- src/main/java/br/com/recipe/service/ReceitaService.java
+++ src/main/java/br/com/recipe/service/ReceitaService.java
@@ -1,53 +1,44 @@
 package br.com.recipe.service;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.recipe.dao.IngredienteDAO;
 import br.com.recipe.dao.ReceitaDAO;
 import br.com.recipe.model.Receita;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReceitaService {
 
-	private ReceitaDAO receitaDAO;
-	private IngredienteDAO ingredienteDAO;
+    private ReceitaDAO receitaDAO;
+    private IngredienteDAO ingredienteDAO;
 
-	public ReceitaService() throws SQLException, ClassNotFoundException {
-		this.receitaDAO = getReceitaDAO();
-		this.ingredienteDAO = getIngredienteDAO();
-	}
+    public ReceitaService(ReceitaDAO receitaDAO, IngredienteDAO ingredienteDAO) {
+        this.receitaDAO = receitaDAO;
+        this.ingredienteDAO = ingredienteDAO;
+    }
 
-	public List<Receita> buscaReceitas() {
-		try {
+    public List<Receita> buscaReceitas() {
+        try {
 
-			List<Receita> receitas = new ArrayList<Receita>();
-			receitas = receitaDAO.lista();
-			return receitas;
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
-		}
-	}
-	
-	public boolean CriaReceita(String receitaId) {
-		try{
+            List<Receita> receitas = new ArrayList<Receita>();
+            receitas = receitaDAO.lista();
+            return receitas;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
 
-			Receita receita = receitaDAO.busca(receitaId);
-			return true;
-		}catch(Exception e) {
-			System.out.println(e);
-			return false;
-		}
+    public boolean CriaReceita(String receitaId) {
+        try {
 
-	}
+            Receita receita = receitaDAO.busca(receitaId);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
 
-	protected static ReceitaDAO getReceitaDAO() throws SQLException, ClassNotFoundException {
-		return new ReceitaDAO();
-	}
-
-	protected static IngredienteDAO getIngredienteDAO() throws SQLException, ClassNotFoundException {
-		return new IngredienteDAO();
-	}
+    }
 
 }
