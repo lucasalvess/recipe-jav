01ab48e9d9ca89245c41fe0ad64cf1ac66c8bd58
lucasalvess <lcluphus@gmail.com>
2021-01-25 16:38:28 -0300

    Testing criaReceita using spy

--- src/test/java/br/com/recipe/service/ReceitaServiceTest.java
+++ src/test/java/br/com/recipe/service/ReceitaServiceTest.java
@@ -1,9 +1,15 @@
 package br.com.recipe.service;
 
-import br.com.recipe.dao.*;
+import br.com.recipe.dao.ConexaoStub;
+import br.com.recipe.dao.IngredienteDaoStub;
+import br.com.recipe.dao.ReceitaDAO;
+import br.com.recipe.dao.ReceitaDaoStub;
 import br.com.recipe.model.Receita;
+import br.com.recipe.model.Usuario;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -13,7 +19,7 @@ public class ReceitaServiceTest {
     private ReceitaDaoStub receitaDaoStub;
     private IngredienteDaoStub ingredienteDaoStub;
     private ReceitaService receitaService;
-    private ConexaoStub conexaoStub;
+    private final ConexaoStub conexaoStub;
 
     public ReceitaServiceTest() {
         conexaoStub = new ConexaoStub();
@@ -30,6 +36,16 @@ public class ReceitaServiceTest {
     public void buscaReceitas_emptyList_thenReturnEmptyList(){
         List<Receita> receitas = receitaService.buscaReceitas();
         assert receitas.isEmpty();
+    }
+
+    @Test
+    public void criaReceita_ValidReceita_thenCallSave() throws SQLException {
+        Usuario usuario = new Usuario();
+        Receita receita = new Receita();
+
+        receitaService.criaReceita(receita,usuario);
+
+        Mockito.spy(receitaDaoStub).salva(receita,usuario);
     }
 
 }
