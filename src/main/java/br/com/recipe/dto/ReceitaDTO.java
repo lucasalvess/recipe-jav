8b60cb9b799d0a9711c8347da5263130d3f59edf
lucasalvess <lcluphus@gmail.com>
2021-03-06 19:13:34 -0300

    Creating ReceitaDTO constructor to convert Receita

--- src/main/java/br/com/recipe/dto/ReceitaDTO.java
+++ src/main/java/br/com/recipe/dto/ReceitaDTO.java
@@ -1,7 +1,7 @@
 package br.com.recipe.dto;
 
 import br.com.recipe.model.Ingrediente;
-import br.com.recipe.model.Usuario;
+import br.com.recipe.model.Receita;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,8 +17,18 @@ public class ReceitaDTO {
     private int id;
     private String nome;
     private String descricao;
-    private String Categoria;
+    private String categoria;
     private String passos;
     private String tempo;
     private List<Ingrediente> ingredientes;
+
+    public ReceitaDTO(Receita receita){
+        this.id = receita.getId();
+        this.nome = receita.getNome();
+        this.categoria = receita.getCategoria();
+        this.descricao = receita.getDescricao();
+        this.passos = receita.getPassos();
+        this.tempo = receita.getTempo();
+        this.ingredientes = receita.getIngredientes();
+    }
 }
