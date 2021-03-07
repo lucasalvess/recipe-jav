41900266e22c073c98c80902f7c127e83b157db4
lucasalvess <lcluphus@gmail.com>
2021-03-07 00:25:34 -0300

    Adding constructors in Ingredient entity

--- src/main/java/br/com/recipe/model/Ingrediente.java
+++ src/main/java/br/com/recipe/model/Ingrediente.java
@@ -2,20 +2,30 @@ package br.com.recipe.model;
 
 public class Ingrediente {
 	
-	private int id;
+	private Integer id;
 	private String nome;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
+
+	public Ingrediente(Integer id, String nome) {
 		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+	public Ingrediente() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
