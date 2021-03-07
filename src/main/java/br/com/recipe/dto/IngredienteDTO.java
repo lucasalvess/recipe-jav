1c60841c0a64a9b7dbe1e76d686748bc9f0b58a1
lucasalvess <lcluphus@gmail.com>
2021-03-07 00:24:07 -0300

    Creating dto response objects and map entities to dto

--- src/main/java/br/com/recipe/dto/IngredienteDTO.java
+++ src/main/java/br/com/recipe/dto/IngredienteDTO.java
@@ -1,4 +1,18 @@
 package br.com.recipe.dto;
 
+import br.com.recipe.model.Ingrediente;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class IngredienteDTO {
+
+    private Integer id;
+    private String nome;
+
+    public IngredienteDTO(Ingrediente ingrediente){
+        this.id = ingrediente.getId();
+        this.nome = ingrediente.getNome();
+    }
 }
--- src/main/java/br/com/recipe/dto/ReceitaDTO.java
+++ src/main/java/br/com/recipe/dto/ReceitaDTO.java
@@ -1,6 +1,5 @@
 package br.com.recipe.dto;
 
-import br.com.recipe.model.Ingrediente;
 import br.com.recipe.model.Receita;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -20,15 +20,20 @@ public class ReceitaDTO {
     private String categoria;
     private String passos;
     private String tempo;
-    private List<Ingrediente> ingredientes;
+    private List<IngredienteDTO> ingredientes;
 
-    public ReceitaDTO(Receita receita){
+    public ReceitaDTO(Receita receita) {
         this.id = receita.getId();
         this.nome = receita.getNome();
         this.categoria = receita.getCategoria();
         this.descricao = receita.getDescricao();
         this.passos = receita.getPassos();
         this.tempo = receita.getTempo();
-        this.ingredientes = receita.getIngredientes();
+        this.ingredientes = (receita.getIngredientes() != null)
+                ? receita.getIngredientes()
+                    .stream()
+                    .map(IngredienteDTO::new)
+                    .collect(Collectors.toList())
+                : null;
     }
 }
